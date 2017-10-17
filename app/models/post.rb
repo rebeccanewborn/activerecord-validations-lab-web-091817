45033f8dcac30193da9f4d4c5239fa8145ee408c
rebeccanewborn <rjn58@cornell.edu>
2017-10-17 10:17:10 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: { maximum: 250 }
+  validates :content, length: { minimum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :is_clickbait?
+
+  def is_clickbait?
+    array = ["Won't Believe", "Secret", "Guess", "Top"]
+    array.any? { |term| self.title.to_s.include?(term)} || errors.add(:title, "isn't clickbait")
+  end
 end
